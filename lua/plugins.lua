165d9c5aa88d3213e5e3111d5206dd513963edfa
xuyundong <xuyundong@douban.com>
2022-10-21 15:30:11 +0800

    change fonts

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,8 +13,8 @@ return require('packer').startup(function()
 
   -- nvim-tree
   use {
-      'kyazdani42/nvim-tree.lua',
-      requires = 'kyazdani42/nvim-web-devicons',
+      'nvim-tree/nvim-tree.lua',
+      requires = 'nvim-tree/nvim-web-devicons',
       config = function() require'nvim-tree'.setup {} end
   }
 
